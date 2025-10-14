c05eaed889000720ac5f6703c7d8e2dd83b40e45
kawanneramos <161034609+kawanneramos@users.noreply.github.com>
2025-10-14 11:08:44 -0300

    14-10-2

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const router = require('./src/routes/routes');
 const app = express(); 
 app.use(cors()); 
 app.use(express.json()); 
-
 app.use(router);
+
+app.use('/public', express.static('public'));
+
 
 const porta = process.env.PORT || 3333;
 
